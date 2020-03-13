adb87bf52b2f16d3385df9918196ad442851e57e
Polina Kuznetsova <kuznetsovapolina@mail.ru>
2020-03-13 23:00:42 +0300

    added ListNode class

--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,23 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    private E payload;
+    private ListNode<E> next;
+
+    public ListNode(E payload, ListNode<E> next) {
+        this.payload = payload;
+        this.next = next;
+    }
+
+    public ListNode<E> getNext() {
+        return next;
+    }
+
+    public E getPayload() {
+        return payload;
+    }
+
+    public void setNext(ListNode<E> next) {
+        this.next = next;
+    }
 }
